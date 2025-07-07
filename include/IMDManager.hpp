8f0227b8136b24408c56fe69d1db40cf5cae40c0
iv <randybenq@gmail.com>
2025-07-07 23:42:33 +0300

    Interface of MDManager got some getters

--- include/IMDManager.hpp
+++ include/IMDManager.hpp
@@ -2,10 +2,12 @@
 #include <Utility.hpp>
 #include <type_traits>
 #include <vector>
-template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+template <typename T, typename = std::enable_if_t<std::is_floating_point_v<T>>>
 class IMDManager {
 public:
-  T getPressure() const noexcept = 0;
-  T getTemperature() const noexcept = 0;
-  std::vector<Particle<T>> &getParticles() const noexcept = 0;
+  virtual T getCurrentPressure() const noexcept = 0;
+  virtual T getCurrentTemperature() const noexcept = 0;
+  virtual std::vector<Particle<T>> &getParticles() const noexcept = 0;
+
+  virtual T getTimeStep() const noexcept = 0;
 };
