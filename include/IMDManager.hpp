aa02058f59809047a50f97eaa8bd42d36c4040d2
iv <randybenq@gmail.com>
2025-07-18 01:27:26 +0300

    Updated IMDManager interface

--- include/IMDManager.hpp
+++ include/IMDManager.hpp
@@ -6,8 +6,21 @@ template <typename T, typename = std::enable_if_t<std::is_floating_point_v<T>>>
 class IMDManager {
 public:
   virtual T getCurrentPressure() const noexcept = 0;
-  virtual T getCurrentTemperature() const noexcept = 0;
+
+  // HINT: mainly for thermostats
+  virtual T getThermostatGroupTemperature(int) const noexcept = 0;
+  virtual const Vector3x<T> &getThermostatGroupVelCom(int) const noexcept = 0;
+  virtual int getThermostatGroupSize(int) const noexcept = 0;
+
   virtual std::vector<Particle<T>> &getParticles() const noexcept = 0;
 
   virtual T getTimeStep() const noexcept = 0;
+  virtual void updateForces() = 0;
+
+  virtual int getFirstGhostIndex() const noexcept = 0;
+  virtual Vector3x<T> getExternalForce() const noexcept = 0;
+
+  // HINT: this function returns true for first iteration after particles
+  // redistribution for afterwards using that information in potentials
+  virtual bool isFirstAfterRebuilt() const noexcept = 0;
 };
