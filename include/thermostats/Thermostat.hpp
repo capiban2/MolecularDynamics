dc26a984f017538852156de06018df93d80a2f40
iv <randybenq@gmail.com>
2025-07-18 01:24:19 +0300

    Prerelease version of Base Thermostat

--- include/thermostats/Thermostat.hpp
+++ include/thermostats/Thermostat.hpp
@@ -9,6 +9,7 @@ class Thermostat {
 protected:
   std::unique_ptr<IMDManager<T>> m_parent;
 
+  T m_half_step_kinetic = 0, m_full_step_kinetic = 0;
   /*
 HINT: this vector contains pairs (int, T),
 where int is a count if iterations, and
@@ -24,25 +25,100 @@ increase to some big arbitary value.
 
   T m_time_step;
 
+  // HINT: necessary for systems with hybrid thermostating, to get temperature
+  // of proper group
+  // this id should be installed by MDManager
+  int m_thermostat_group_id;
+
+  T calculateKinetic(T *velx, T *vely, T *velz, const Vector3x<T> &vcom,
+                     double *mass, int first_ghost) {
+    T kinetic = 0.;
+    T dx, dy, dz;
+#ifdef OPENMP_ENABLED
+#pragma omp parallel for reduction(+ : kinetic)
+#endif
+    for (int t_ = 0; t_ != first_ghost; ++t_) {
+      dx = velx[t_] - vcom.x;
+      dy = vely[t_] - vcom.y;
+      dz = velz[t_] - vcom.z;
+      kinetic += mass[t_] * 0.5 * (dx * dx + dy * dy + dz * dz);
+    }
+    return kinetic;
+  }
+  T calculateKinetic(T *velx, T *vely, T *velz, const Vector3x<T> &vcom,
+                     double *mass, const std::vector<int> &idx) {
+    T kinetic = 0.;
+    T dx, dy, dz;
+#ifdef OPENMP_ENABLED
+#pragma omp parallel for reduction(+ : kinetic)
+#endif
+    for (int t_ = 0; t_ != idx.size(); ++t_) {
+      dx = velx[idx[t_]] - vcom.x;
+      dy = vely[idx[t_]] - vcom.y;
+      dz = velz[idx[t_]] - vcom.z;
+      kinetic += mass[t_] * 0.5 * (dx * dx + dy * dy + dz * dz);
+    }
+    return kinetic;
+  }
+
 public:
-  enum class Type { Unitary, Andersen, Berendsen, Langevin, NoseHoover };
+  enum class Type { Identity, Andersen, Berendsen, Langevin, NoseHoover };
+  Thermostat() = delete;
+  virtual ~Thermostat() = default;
   Thermostat(IMDManager<T> *mdm,
-             const std::vector<std::pair<int, T>> &_preserve_temp)
-      : m_parent(mdm), m_time_nd_temp_to_preserve(_preserve_temp) {
+             const std::vector<std::pair<int, T>> &_preserve_temp, int group_id)
+      : m_parent(mdm), m_time_nd_temp_to_preserve(_preserve_temp),
+        m_thermostat_group_id(group_id) {
     m_time_step = m_parent->getTimeStep();
   }
-  virtual void applyThermostat() = 0;
 
+  void setData(IMDManager<T> *mdm,
+               const std::vector<std::pair<int, T>> &_preserve_temp) noexcept {
+    m_parent = mdm;
+    m_time_nd_temp_to_preserve = _preserve_temp;
+    m_time_step = m_parent->getTimeStep();
+  }
   // HINT: becauase dynamic target temperature has been supported, then, it may
   // return different target temperatures, that depend on current timestemp
   virtual T getTargetTemperature() const noexcept;
+  virtual T getHalfStepTemperature() const noexcept;
+  virtual T getFullStepTemperature() const noexcept;
+
+  // HINT: for global thermostat
+  virtual void applyThermostat(ParticleData<T> &_data,
+                               const std::vector<double> &mass,
+                               bool half_step) = 0;
+  // HINT: for local thermostat
+  virtual void applyThermostat(ParticleData<T> &_data,
+                               const std::vector<int> &_idx,
+                               const std::vector<double> &mass,
+                               bool half_step) = 0;
+
+  // HINT: for global thermostat; for calculating current system's
+  // temperature(kinetic temperature) to work with it afterwards
+  virtual void initThermostat(ParticleData<T> &_data,
+                              const std::vector<double> &mass) = 0;
+  // HINT: for local thermostat; for calculating current system's
+  // temperature(kinetic temperature) to work with it afterwards
+  virtual void initThermostat(ParticleData<T> &_data,
+                              const std::vector<int> &_idx,
+                              const std::vector<double> &mass) = 0;
 };
 template <typename T, typename = std::enable_if_t<std::is_floating_point_v<T>>>
-class UnitaryThermostat final : public Thermostat<T> {
+class IdentityThermostat final : public Thermostat<T> {
 
 public:
-  UnitaryThermostat(IMDManager<T> *mdm) : Thermostat<T>(mdm) {}
-  virtual void applyThermostat() override {}
+  IdentityThermostat(IMDManager<T> *mdm,
+                     const std::vector<std::pair<int, T>> &_preserve_temp,
+                     int group_id)
+      : Thermostat<T>(mdm, _preserve_temp, group_id) {}
+
+  virtual void applyThermostat(ParticleData<T> &_data, bool to_half_velocity) {}
+
+  virtual void applyThermostat(ParticleData<T> &_data,
+                               const std::vector<int> &_idx,
+                               bool to_half_velocity) override {}
+
   virtual T getTargetTemperature() const noexcept override {
     return this->m_parent->getCurrentTemperature();
   }
